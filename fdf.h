c7eb4953a18ab2d58d1ab0d9bb71f4d5a91c4119
Bongani NKOSI <bnkosi@c5r3s5.wethinkcode.co.za>
2019-08-18 10:55:11 +0200

    safety push, starting fdf

---
+++ fdf.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnkosi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/18 10:46:25 by bnkosi            #+#    #+#             */
+/*   Updated: 2019/08/18 10:51:37 by bnkosi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+#include "libft/libft.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <math.h>
+#include "./minilix/mlx.h"
+
+thpedef struct	f_map
+{
+int				**map;
+void			*mlx;
+void			*win;
+int				color;
+int				w;
+int				h;
+int				winwidth;
+int				winheight;
+int				**xgrid;
+int				**ygrid;
+int				dx;
+int				dy;
+int				d;
+int				x1;
+int				y1;
+int				x2;
+int				y2;
+int				sign1;
+int				sign2;
+int				x;
+int				y;
+int				i;
+int				swapit;
+int				width;
+int				height;
+}				f_map;
+
+#endif
