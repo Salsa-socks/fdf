c009ab0347cf81df3797e03bc05a98f179138cea
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-08-29 06:39:57 +0200

    color change working

--- make_window.c
+++ make_window.c
@@ -61,6 +61,7 @@ void	make_window(t_map *fmap)
 	fmap->mlx = mlx_init();
 	fmap->window = mlx_new_window(fmap->mlx, fmap->w_width, fmap->w_height, "FDF");
 	mlx_key_hook(fmap->window, escclose, fmap);
+	mlx_key_hook(fmap->window, color_key, fmap);
 	fmap->xgrid = creategrid(fmap);
 	fmap->ygrid = creategrid(fmap);
 	create_box(fmap);
